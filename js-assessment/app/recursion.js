5ea5a9faab816d58e1017173af49145940381e64
Mariano Pardo <marianomdq1@gmail.com>
2014-04-01 16:10:02 -0300

    Completed recursion answers

--- js-assessment/app/recursion.js
+++ js-assessment/app/recursion.js
@@ -3,11 +3,37 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
+      var results = [];
 
+      var findFiles = function findFiles (data, dirName, directoryMatch) {
+        if (data.files) {
+          directoryMatch |= data.dir === dirName;
+          for (var i = 0, len = data.files.length; i < len; i++) {
+            findFiles(data.files[i], dirName, directoryMatch);
+          }
+        } else if (directoryMatch) {
+          results.push(data);
+        }
+      }(data, dirName, !dirName);
+
+      return results;
     },
 
     permute: function(arr) {
-
+      var result = [];
+      var visited = [];
+      var permute = function permute (arr) {
+        if (~visited.indexOf(arr.join(','))) {
+          return;
+        }
+        result.push(arr);
+        visited.push(arr.join(','));
+        for (var i = 0, len = arr.length; i < len; i++) {
+          arr[i] = arr.splice(i - 1, 1, arr[i])[0];
+          permute([].slice.call(arr));
+        }
+      }(arr);
+      return result;
     }
   };
 });
