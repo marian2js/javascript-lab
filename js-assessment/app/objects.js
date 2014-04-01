99a37edbfb55e690ea54346c7a391161aa3c1312
Mariano Pardo <marianomdq1@gmail.com>
2014-04-01 15:03:45 -0300

    Completed objects answers

--- js-assessment/app/objects.js
+++ js-assessment/app/objects.js
@@ -3,15 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.apply(obj, arguments);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+      var result = [];
+      for (var key in obj) {
+        if (!obj.hasOwnProperty(key)) {
+          continue;
+        }
+        result.push(key + ': ' + obj[key]);
+      }
+      return result;
     }
   };
 });
