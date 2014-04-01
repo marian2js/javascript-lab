124f50d2130d87a622e26abb6554737edf98e33b
Mariano Pardo <marianomdq1@gmail.com>
2014-04-01 14:19:38 -0300

    Completed modules answers

--- js-assessment/app/modules.js
+++ js-assessment/app/modules.js
@@ -3,7 +3,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
-
+      return (function (greeting, name) {
+        var self = {};
+        self.greeting = greeting;
+        self.name = name;
+        self.sayIt = function () {
+          return self.greeting + ', ' + self.name;
+        };
+        return self;
+      })(str1, str2);
     }
   };
 });
--- js-assessment/app/regex.js
+++ js-assessment/app/regex.js
@@ -3,15 +3,18 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+      var regex = /[0-9]+/;
+      return regex.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+      var regex = /([a-z])\1/i;
+      return regex.test(str);
     },
 
     endsWithVowel : function(str) {
-
+      var regex = /.*[a|e|i|o|u]$/;
+      return regex.test(str);
     },
 
     captureThreeNumbers : function(str) {
@@ -19,7 +22,8 @@ define(function() {
     },
 
     matchesPattern : function(str) {
-
+      var regex = /(.*){3}-(.*){3}-(.*){3}/;
+      return regex.test(str);
     },
     isUSD : function(str) {
 
