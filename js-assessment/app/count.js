dfcd8855732368f3cf4f8f335f291697220166f7
Mariano Pardo <marianomdq1@gmail.com>
2014-04-01 12:49:24 -0300

    Completed count answers

--- js-assessment/app/count.js
+++ js-assessment/app/count.js
@@ -3,6 +3,30 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var logNextNumber,
+          interval,
+          cancelInterval;
+
+      logNextNumber = function () {
+        console.log(start++);
+      };
+
+      // print the next number every 100ms starting now
+      logNextNumber();
+      interval = setInterval(function () {
+        logNextNumber();
+        if (start > end) {
+          cancelInterval();
+        }
+      }, 100);
+
+      cancelInterval = function () {
+        clearInterval(interval);
+      };
+
+      return {
+        cancel: cancelInterval
+      }
     }
   };
 });
