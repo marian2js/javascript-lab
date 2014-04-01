50c5a6ee67e9f0b62188a4f672e8ec7c2dbbe48b
Mariano Pardo <marianomdq1@gmail.com>
2014-04-01 17:00:05 -0300

    Completed regex answers

--- js-assessment/app/regex.js
+++ js-assessment/app/regex.js
@@ -13,20 +13,24 @@ define(function() {
     },
 
     endsWithVowel : function(str) {
-      var regex = /.*[a|e|i|o|u]$/;
+      var regex = /[aeiou]$/i;
       return regex.test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+      var regex = /\d{3}/i;
+      var matches = regex.exec(str);
+      return Array.isArray(matches) ? matches[0] : false;
     },
 
     matchesPattern : function(str) {
-      var regex = /(.*){3}-(.*){3}-(.*){3}/;
+      var regex = /^(\d){3}-(\d){3}-(\d){4}$/;
       return regex.test(str);
     },
-    isUSD : function(str) {
 
+    isUSD : function(str) {
+      var regex = /^\$(\d){1,3}((,(\d){3})*(\.(\d){2}){0,1})*$/;
+      return regex.test(str);
     }
   };
 });
