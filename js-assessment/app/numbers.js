2cc0e02156214ef77c0d87502920452a82511b1c
Mariano Pardo <marianomdq1@gmail.com>
2014-04-01 14:41:08 -0300

    Completed numbers answers

--- js-assessment/app/numbers.js
+++ js-assessment/app/numbers.js
@@ -3,19 +3,30 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+      var binaryArr = num.toString(2).split('').reverse();
+      var strBit = binaryArr[bit - 1];
+      return parseInt(strBit);
     },
 
     base10: function(str) {
-
+      var strNum = parseInt(str, 2).toString(10);
+      return parseInt(strNum, 10)
     },
 
     convertToBinary: function(num) {
-
+      var strNum = parseInt(num).toString(2);
+      for (var i = strNum.length; i < 8; i++) {
+        strNum = '0' + strNum;
+      }
+      return strNum
     },
 
     multiply: function(a, b) {
-
+      var digits = 4;
+      var result = a * b;
+      var fixed4 = result.toFixed(digits);
+      var exp = Math.pow(10, digits);
+      return Math.round(fixed4 * exp) / exp;
     }
   };
 });
