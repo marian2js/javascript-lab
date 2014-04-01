0d1f38b51ba7c3b18f84d650f6ed7157511d63d4
Mariano Pardo <marianomdq1@gmail.com>
2014-04-01 12:20:02 -0300

    Completed async answers

--- js-assessment/app/async.js
+++ js-assessment/app/async.js
@@ -3,11 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+      return $.Deferred().resolve(value).promise();
     },
 
     manipulateRemoteData : function(url) {
-
+      var defer = $.Deferred();
+      $.get(url)
+        .then(function (data) {
+          data = data.people.map(function (people) {
+            return people.name;
+          }).sort();
+          defer.resolve(data);
+        });
+      return defer.promise();
     }
   };
 });
