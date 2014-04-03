ae4afa39b3c0c177920985d991a7c2921116ec23
Mariano Pardo <marianomdq1@gmail.com>
2014-04-03 11:15:05 -0300

    Added tests for async returns in chained promises

--- src/q/test/q.spec.js
+++ src/q/test/q.spec.js
@@ -1,5 +1,4 @@
 describe('Q', function () {
-  var jasmine = this;
 
   /**
    * Async function that success if val is true or fails otherwise
@@ -13,12 +12,15 @@ describe('Q', function () {
       if (val) {
         defer.resolve(true);
       } else {
-        defer.reject('It should never be here');
+        defer.reject('This is an expected error');
       }
     }, 100);
     return defer.promise;
   };
 
+  /**
+   * Test if then is called and resolved value is sent to it
+   */
   it('should call then method on success', function (done) {
    async(true)
       .then(function (val) {
@@ -26,15 +28,17 @@ describe('Q', function () {
         done();
       })
       .fail(function () {
-        jasmine.fail(Error('It should never be here'));
+        throw new Error('It should never be here');
       });
   });
 
+  /**
+   * Test if fail is called and rejected error is sent to it
+   */
   it('should call fail method on error', function (done) {
     async(false)
       .then(function () {
-        jasmine.fail(Error('It should never be here'));
-        done();
+        throw new Error('It should never be here');
       })
       .fail(function (err) {
         expect(err).toBe('This is an expected error');
@@ -42,6 +46,9 @@ describe('Q', function () {
       });
   });
 
+  /**
+   * Test if complete is called on resolve
+   */
   it('should call complete on success', function (done) {
     async(true)
       .then(function (val) {
@@ -50,6 +57,9 @@ describe('Q', function () {
       .complete(done);
   });
 
+  /**
+   * Test if complete is called on reject
+   */
   it('should call complete on error', function (done) {
     async(false)
       .fail(function (err) {
@@ -58,23 +68,61 @@ describe('Q', function () {
       .complete(done);
   });
 
+  /**
+   * Test if chained thens are called on resolve
+   */
   it('should chain multiple "thens"', function (done) {
     async(true)
-      .then(function (val) {
-        expect(val).toBe(true);
-
+      .then(function () {
         return 'value';
       })
       .then(function (val) {
         expect(val).toBe('value');
-
         return 'value 2';
       })
       .then(function (val) {
         expect(val).toBe('value 2');
       })
       .fail(function () {
-        jasmine.fail(Error('It should never be here'));
+        throw new Error('It should never be here');
+      })
+      .complete(done);
+  });
+
+  /**
+   * Test if thens can return a Promise and the next then receive the value
+   */
+  it('should allow "thens" to return a Promise', function (done) {
+    async(true)
+      .then(function () {
+        return async(true);
+      })
+      .then(function (val) {
+        expect(val).toBe(true);
+        return async(true);
+      })
+      .then(function (val) {
+        expect(val).toBe(true);
+      })
+      .fail(function () {
+        throw new Error('It should never be here');
+      })
+      .complete(done);
+  });
+
+  /**
+   * Test if a fail is called when a chained then returns a failed Promise
+   */
+  it('should call a successful Promise and then a failed one', function (done) {
+    async(true)
+      .then(function () {
+        return async(false);
+      })
+      .then(function () {
+        throw new Error('It should never be here');
+      })
+      .fail(function (error) {
+        expect(error).toBe('This is an expected error');
       })
       .complete(done);
   });
